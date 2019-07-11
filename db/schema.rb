23af8495321c562dad739c7f4cd4caf2bf7f6ffe
Andy Feidje Joseph <ingandy75@gmail.com>
2019-07-11 16:44:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_07_11_153805) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
+
 end
