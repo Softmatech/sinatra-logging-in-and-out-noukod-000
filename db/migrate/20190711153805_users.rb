b4e6ed814a052bd099362334da1495a49a68f8c7
Andy Feidje Joseph <ingandy75@gmail.com>
2019-07-11 16:44:24 +0000

    Automatically backed up by Learn

--- db/migrate/20190711153805_users.rb
+++ db/migrate/20190711153805_users.rb
@@ -1,6 +1,6 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
-    create_table users do |u|
+    create_table :users do |u|
       u.string :username
       u.string :password
       u.decimal :balance
